ea9703718fe120b0896114770f8310dbb6a42459
William Smith <williamprincesmith@gmail.com>
2024-03-27 16:53:43 -0700

    [formal-snapshots] Introduce strict verification

--- crates/sui-tool/src/lib.rs
+++ crates/sui-tool/src/lib.rs
@@ -36,11 +36,13 @@ use tokio::task::JoinHandle;
 use tokio::time::Instant;
 
 use anyhow::anyhow;
+use clap::ValueEnum;
 use eyre::ContextCompat;
 use fastcrypto::hash::MultisetHash;
 use futures::{StreamExt, TryStreamExt};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use prometheus::Registry;
+use serde::{Deserialize, Serialize};
 use sui_archival::reader::{ArchiveReader, ArchiveReaderMetrics};
 use sui_archival::{verify_archive_with_checksums, verify_archive_with_genesis_config};
 use sui_config::node::ArchiveReaderConfig;
@@ -68,6 +70,19 @@ use typed_store::rocks::MetricConf;
 pub mod commands;
 pub mod db_tool;
 pub mod pkg_dump;
+
+#[derive(
+    Clone, Serialize, Deserialize, Debug, PartialEq, Copy, PartialOrd, Ord, Eq, ValueEnum, Default,
+)]
+pub enum SnapshotVerifyMode {
+    /// verification of both db state and checkpoint chain is skipped.
+    None,
+    /// verify snapshot state during download, but no post-restore db verification.
+    #[default]
+    Normal,
+    /// verify db state post-restore against the end of epoch state root commitment.
+    Strict,
+}
 
 // This functions requires at least one of genesis or fullnode_rpc to be `Some`.
 async fn make_clients(
@@ -850,7 +865,7 @@ pub async fn download_formal_snapshot(
     archive_store_config: ObjectStoreConfig,
     num_parallel_downloads: usize,
     network: Chain,
-    verify: bool,
+    verify: SnapshotVerifyMode,
 ) -> Result<(), anyhow::Error> {
     eprintln!(
         "Beginning formal snapshot restore to end of epoch {}, network: {:?}",
@@ -885,7 +900,7 @@ pub async fn download_formal_snapshot(
         archive_store_config.clone(),
         epoch,
         num_parallel_downloads,
-        verify,
+        verify != SnapshotVerifyMode::None,
     );
     let (_abort_handle, abort_registration) = AbortHandle::new_pair();
     let perpetual_db_clone = perpetual_db.clone();
@@ -935,7 +950,7 @@ pub async fn download_formal_snapshot(
         .expect("Expected nonempty checkpoint store");
 
     // Perform snapshot state verification
-    if verify {
+    if verify != SnapshotVerifyMode::None {
         assert_eq!(
             last_checkpoint.epoch(),
             epoch,
@@ -962,7 +977,7 @@ pub async fn download_formal_snapshot(
                 assert_eq!(
                     *consensus_digest, local_digest,
                     "End of epoch {} root state digest {} does not match \
-                    local root state hash {} after restoring from formal snapshot",
+                    local root state hash {} computed from snapshot data",
                     epoch, consensus_digest.digest, local_digest.digest,
                 );
                 eprintln!("Formal snapshot state verification completed successfully!");
@@ -988,10 +1003,12 @@ pub async fn download_formal_snapshot(
 
     setup_db_state(
         epoch,
-        root_accumulator,
-        perpetual_db,
+        root_accumulator.clone(),
+        perpetual_db.clone(),
         checkpoint_store,
         committee_store,
+        network,
+        verify == SnapshotVerifyMode::Strict,
     )
     .await?;
 
